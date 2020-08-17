8e8267478bf841619041fadd0b6a634507989fd9
xuedong <dong_flying@163.com>
2020-08-17 16:07:30 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <a href="https://learnku.com/laravel/courses" target=_blank>
+        刻意练习，每日精进
+    </a>
+    <div class="float-right">
+        <a href="{{ route('home') }}">首页</a> |
+        <a href="{{ route('help') }}">帮助</a> |
+        <a href="{{ route('about') }}">关于</a>
+    </div>
+</footer>
