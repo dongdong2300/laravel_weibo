c664bde3dbe5b293625d3ab0a665ac14a7702fde
xuedong <dong_flying@163.com>
2020-08-17 20:38:05 +0800

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -45,14 +45,28 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
-    public function edit($id)
+    public function edit(User $user)
     {
-        //
+        return view('users.edit', compact('user'));
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
-        //
+        $this->validate($request, [
+            'name'     => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data         = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success','个人资料更新成功！');
+
+        return redirect()->route('users.show',$user);
     }
 
     public function destroy($id)
