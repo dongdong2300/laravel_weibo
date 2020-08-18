610d03abb821d3bc747c9c6e956ee2e49c89272c
xuedong <dong_flying@163.com>
2020-08-18 21:40:22 +0800

    关注、取消关注 按钮

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,6 +6,9 @@
         <div class="offset-md-2 col-md-8">
             <section class="user_info">
                 @include('shared._user_info', ['user' => $user])
+                @if (Auth::check())
+                    @include('users._follow_form')
+                @endif
             </section>
             <section class="stats mt-2">
                 @include('shared._stats', ['user' => $user])
