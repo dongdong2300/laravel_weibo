d74ea73cb467797ee8b23251aa4ee90699350b37
xuedong <dong_flying@163.com>
2020-08-18 21:29:04 +0800

    社交统计信息

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -4,18 +4,19 @@
     @if (Auth::check())
         <div class="row">
             <div class="col-md-8">
-                <div class="col-md-8">
-                    <section class="status_form">
-                        @include('shared._status_form')
-                    </section>
-                    <h4>微博列表</h4>
-                    <hr>
-                    @include('shared._feed')
-                </div>
+                <section class="status_form">
+                    @include('shared._status_form')
+                </section>
+                <h4>微博列表</h4>
+                <hr>
+                @include('shared._feed')
             </div>
             <aside class="col-md-4">
                 <section class="user_info">
                     @include('shared._user_info', ['user' => Auth::user()])
+                </section>
+                <section class="stats mt-2">
+                    @include('shared._stats', ['user' => Auth::user()])
                 </section>
             </aside>
         </div>
